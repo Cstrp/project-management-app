5dda8c93c99c1f86e658ec3945a0b0747f15793a
Cstrp <valery.nosareu@gmail.com>
2022-11-11 12:39:48 +0300

    feat: add local storage..

---
+++ src/app/services/local-storage.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { _decrypt, _encrypt } from '../constants';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class LocalStorageService {
+  constructor() {}
+
+  public saveData(key: string, value: string): void {
+    localStorage.setItem(key, _encrypt(value));
+  }
+
+  public getData(key: string): string {
+    const data = localStorage.getItem(key) || '';
+
+    return _decrypt(data);
+  }
+
+  public removeData(): void {
+    localStorage.clear();
+  }
+
+  public removeItem(key: string): void {
+    localStorage.removeItem(key);
+  }
+}
