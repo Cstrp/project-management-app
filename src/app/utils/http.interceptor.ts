1a1a6bbe37cf67be6b42a5cd11d76e5c0fc0cfd9
[Valentina] <[mariaiscus1@gmail.com]>
2022-11-12 22:06:58 +0600

    feat: add update and delete boards

--- src/app/utils/http.interceptor.ts
+++ src/app/utils/http.interceptor.ts
@@ -16,7 +16,6 @@ export class HttpRequestInterceptor implements HttpInterceptor {
 
     return req.clone({
       setHeaders: {
-        'Content-Type': 'application/json',
         Authorization:
           // eslint-disable-next-line max-len
           'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJiNTU1NWM2Ni1lZDU5LTQ1NDQtYjgxNS02OWI4ZWRiNGNhYWQiLCJsb2dpbiI6IkFuZHJleSIsImlhdCI6MTY2ODIwMjA4M30.EqLcYzOkQWhzOJt1lBbcDpj1sAn_BqA_oqkbI-nvjNY',
