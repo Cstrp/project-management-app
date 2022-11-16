57a484b704946a45de7b474306a8905ff0dac60d
Cstrp <valery.nosareu@gmail.com>
2022-11-16 16:21:19 +0300

    fix: rename branch

--- src/app/modules/home/components/top-section/top-section.component.ts
+++ src/app/modules/home/components/top-section/top-section.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-top-section',
   templateUrl: './top-section.component.html',
-  styleUrls: ['./top-section.component.scss']
+  styleUrls: ['./top-section.component.scss'],
 })
 export class TopSectionComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
