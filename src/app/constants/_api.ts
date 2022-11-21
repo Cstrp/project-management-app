08fc980fbea96a43d10ebebe9255a4bb7c2808f5
angelina <floresangela.0707@gmail.com>
2022-11-21 21:11:33 +0600

    fix: fix loader

--- src/app/constants/_api.ts
+++ src/app/constants/_api.ts
@@ -1,3 +1,3 @@
+// const BASE_URL: string = 'https://final-task-backend-production-aa14.up.railway.app';
 const BASE_URL: string = 'https://aqueous-oasis-19257.herokuapp.com';
-
 export { BASE_URL };
