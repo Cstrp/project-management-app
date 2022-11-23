481465566659753fd0629f37ccf82e94f88fb92a
Cstrp <valery.nosareu@gmail.com>
2022-11-23 14:37:54 +0300

    fix: revert this branch

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,19 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { DragDropModule } from '@angular/cdk/drag-drop';
-import { StoreModule } from '@ngrx/store';
-import { EffectsModule } from '@ngrx/effects';
-import { StoreRouterConnectingModule } from '@ngrx/router-store';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { environment } from '../environments/environment';
-import { MaterialModule } from './modules/material/material.module';
-import { AuthPageModule } from './modules/auth-page/auth-page.module';
-import { SharedModule } from './modules/shared/shared.module';
-import { HomeModule } from './modules/home/home.module';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {DragDropModule} from '@angular/cdk/drag-drop';
+import {StoreModule} from '@ngrx/store';
+import {EffectsModule} from '@ngrx/effects';
+import {StoreRouterConnectingModule} from '@ngrx/router-store';
+import {StoreDevtoolsModule} from '@ngrx/store-devtools';
+import {environment} from '../environments/environment';
+import {MaterialModule} from './modules/material/material.module';
+import {AuthPageModule} from './modules/auth-page/auth-page.module';
+import {SharedModule} from './modules/shared/shared.module';
+import {HomeModule} from './modules/home/home.module';
 
 @NgModule({
   declarations: [AppComponent],
@@ -40,6 +40,6 @@ import { HomeModule } from './modules/home/home.module';
 export class AppModule {}
 
 
-// https://final-task-backend-production-aa14.up.railway.app/api-docs/ 
+// https://final-task-backend-production-aa14.up.railway.app/api-docs/
 
 // this back is work!
