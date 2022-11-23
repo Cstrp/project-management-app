d8ff633870f4904d034b3042d6010814f8bfe3aa
Cstrp <valery.nosareu@gmail.com>
2022-11-23 15:06:16 +0300

    doc: test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,19 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {DragDropModule} from '@angular/cdk/drag-drop';
-import {StoreModule} from '@ngrx/store';
-import {EffectsModule} from '@ngrx/effects';
-import {StoreRouterConnectingModule} from '@ngrx/router-store';
-import {StoreDevtoolsModule} from '@ngrx/store-devtools';
-import {environment} from '../environments/environment';
-import {MaterialModule} from './modules/material/material.module';
-import {AuthPageModule} from './modules/auth-page/auth-page.module';
-import {SharedModule} from './modules/shared/shared.module';
-import {HomeModule} from './modules/home/home.module';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { AuthPageModule } from './modules/auth-page/auth-page.module';
+import { HomeModule } from './modules/home/home.module';
+import { MaterialModule } from './modules/material/material.module';
+import { SharedModule } from './modules/shared/shared.module';
 
 @NgModule({
   declarations: [AppComponent],
@@ -37,7 +37,7 @@ import {HomeModule} from './modules/home/home.module';
   providers: [],
   bootstrap: [AppComponent],
 })
-export class AppModule {}
+export class AppModule { }
 
 
 // https://final-task-backend-production-aa14.up.railway.app/api-docs/
