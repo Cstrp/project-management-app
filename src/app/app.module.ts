25683d8acb041836c36ee4bae7717524a1695c37
Cstrp <valery.nosareu@gmail.com>
2022-11-23 15:19:40 +0300

    doc: fix error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,25 +1,25 @@
-import {DragDropModule} from '@angular/cdk/drag-drop';
-import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
-import {NgModule} from '@angular/core';
-import {FormsModule} from '@angular/forms';
-import {MatButtonModule} from '@angular/material/button';
-import {BrowserModule} from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {EffectsModule} from '@ngrx/effects';
-import {StoreRouterConnectingModule} from '@ngrx/router-store';
-import {StoreModule} from '@ngrx/store';
-import {StoreDevtoolsModule} from '@ngrx/store-devtools';
-import {environment} from '../environments/environment';
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {BoardsModule} from './modules';
-import {AuthPageModule} from './modules/auth-page/auth-page.module';
-import {HomeModule} from './modules/home/home.module';
-import {MaterialModule} from './modules/material/material.module';
-import {NetworkInterceptor} from './modules/shared/interceptors/network.interceptor';
-import {SharedModule} from './modules/shared/shared.module';
-import {appReducer} from './store';
-import {CustomSerializer} from './store/app/router/custom-serializer';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BoardsModule } from './modules';
+import { AuthPageModule } from './modules/auth-page/auth-page.module';
+import { HomeModule } from './modules/home/home.module';
+import { MaterialModule } from './modules/material/material.module';
+import { NetworkInterceptor } from './modules/shared/interceptors/network.interceptor';
+import { SharedModule } from './modules/shared/shared.module';
+import { appReducer } from './store';
+import { CustomSerializer } from './store/app/router/custom-serializer';
 
 @NgModule({
   declarations: [AppComponent],
@@ -55,10 +55,8 @@ import {CustomSerializer} from './store/app/router/custom-serializer';
   ],
   bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
 
-
-
-// https://final-task-backend-production-aa14.up.railway.app/api-docs/ 
+// https://final-task-backend-production-aa14.up.railway.app/api-docs/
 
 // this back is work!
